27088bd10f4031e54a199d9bf0cfe9831edae627
Atharva Dhanvate <41443365+anonymous-baaka@users.noreply.github.com>
2020-09-29 22:14:26 +0530

    Create connection.php

---
+++ php/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$sql_host="localhost";
+$sql_username="root";
+$sql_password="";
+$sql_database="printonline";
+
+$connection=new mysqli($sql_host,$sql_username,$sql_password,$sql_database);
+
+?>
