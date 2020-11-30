c902568ae071b2f9635b150d99508e4ac649cfa7
Atharva Dhanwate <41443365+anonymous-baaka@users.noreply.github.com>
2020-11-30 10:43:44 +0530

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PrintOnline Web Services</title>
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<h3 id="brand-logo">PrintOnline</h3>
+		<button id="top-button" class="white-button">Log out</button>
+	</header>
+
+	<div id="main-card">
+		<h2>WELCOME DEAR ADMIN !</h2>
+		<h4>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		</h4>
+	</div>
+
+	<div id="left" class="section">
+		<h3>PrintOnline Highlights Yesterday</h3>
+		<div class="card" style="height: 300px; padding: 15px 30px 5px 30px;">
+			<div style="float: left; width: 80%">
+				Total Pages Printed Yesterday<br><br>
+				Pages Printed for Sudents <br><br>
+				Pages Printed for Teachers<br><br>
+				Total Requests made Yesterday<br><br>
+				Requests made by Students<br><br>
+				Requests made by Teachers<br><br>
+			</div>
+			<div style="float: right; width: 15%; text-align: right;">
+				3000<br><br>
+				3000<br><br>
+				3000<br><br>
+				3000<br><br>
+				3000<br><br>
+				3000<br><br>
+			</div>
+		</div>
+	</div>
+
+	<div id="vl"></div>
+	<div id="right" class="section">
+		<h3 style="display: inline-block; float: left; margin-top: 8px;">Validate Requests</h3>&emsp;
+		<button type="submit" name="archives" class="blue-button" style=" border:solid .5px #2C2C96; float: right;">Validate</button>
+		<h5 style="display: inline-block; float: unset; margin: 10px 0px 30px 0px;">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		</h5>
+		<br>
+
+		<h3 style="display: inline-block; float: left; margin-top: 8px;">Make Supplier Request</h3>&emsp;
+		<button type="submit" name="archives" class="blue-button" style=" border:solid .5px #2C2C96; float: right;">Request</button>
+		<h5 style="display: inline-block; float: unset; margin: 10px 0px 30px 0px;">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		</h5>
+		
+	</div>
+
+</body>
+</html>
